9cdc0fa91eec9c26785af5cd692859be0f3d6ad4
vicheath <vicheath.a.s@gmail.com>
2024-01-19 11:28:00 -0500

    error handling

--- index.js
+++ index.js
@@ -1,12 +1,66 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const bodyParser = require("body-parser");
 
-const users = require("./data/users");
-const posts = require("./data/posts");
+
+const users = require("./routes/users");
+const posts = require("./routes/posts");
+const error = require("./utilities/error");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json({ extended: true }));
+
+// Logging Middlewaare
+app.use((req, res, next) => {
+  const time = new Date();
+
+  console.log(
+    `-----
+${time.toLocaleTimeString()}: Received a ${req.method} request to ${req.url}.`
+  );
+  if (Object.keys(req.body).length > 0) {
+    console.log("Containing the data:");
+    console.log(`${JSON.stringify(req.body)}`);
+  }
+  next();
+});
+
+// Valid API Keys.
+apiKeys = ["perscholas", "ps-example", "hJAsknw-L198sAJD-l3kasx"];
+
+
+app.use("/api", function (req, res, next) {
+  var key = req.query["api-key"];
+
+  // Check for the absence of a key.
+  if (!key) next(error(400, "API Key Required"));
+
+  // Check for key validity.
+  if (apiKeys.indexOf(key) === -1) next(error(401, "Invalid API Key"));
+
+  // Valid key! Store it in req.key for route access.
+  req.key = key;
+  next();
+});
+
+app.use((err, req, res, next) => {
+  res.status(err.status || 500);
+  res.json({ error: err.message });
+});
+
+app.listen(port, () => {
+  console.log(`Server listening on port: ${port}.`);
+});
+
+
+
+// Use our Routes
+app.use("/api/users", users);
+app.use("/api/posts", posts);
+
+
+
 
 //ALL Routes
 app
@@ -128,15 +182,15 @@ app
     else next();
   });
 
-app.get("/", (req, res) => {
-  res.send("Work in progress!");
-});
+// app.get("/", (req, res) => {
+//   res.send("Work in progress!");
+// });
 
-app.use((req, res) => {
-  res.status(404);
-  res.json({ error: "Resource Not Found" });
-});
+// app.use((req, res) => {
+//   res.status(404);
+//   res.json({ error: "Resource Not Found" });
+// });
 
-app.listen(port, () => {
-  console.log(`Server listening on port: ${port}.`);
-});
+// app.listen(port, () => {
+//   console.log(`Server listening on port: ${port}.`);
+// });
